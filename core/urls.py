4ead013e0b22f3f400b7bd30b88e92fc06c399e7
syed waheed <waheedsyed291@gmail.com>
2024-01-07 13:39:22 +0530

    Initial commit

---
+++ core/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from.import views
+
+urlpatterns = [
+    path('', views.index, name='index'), 
+    path('settings', views.settings, name='settings'), 
+    path('upload', views.upload, name='upload'), 
+    path('follow', views.follow, name='follow'), 
+    path('search', views.search, name='search'), 
+    path('profile/<str:pk>', views.profile, name='profile'), 
+    path('like-post', views.like_post, name='like-post'), 
+    path('signup', views.signup, name='signup'), 
+    path('signin', views.signin, name='signin'), 
+    path('logout', views.logout, name='logout'), 
+    # path('delete_post/<int:post_id>/',  views.delete_post, name='delete_post'),
+    # path('delete_post/<uuid:post_id>/', views.delete_post, name='delete_post'), 
+    path('delete_post/<uuid:post_id>/', views.delete_post, name='delete_post'),
+
+
+]
